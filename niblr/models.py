7fbadcfb2e467c189bba2fa826e0cdfe14b87d0d
Seun Dawodu <seun.dawodu@artsalliancemedia.com>
2017-03-13 10:11:54 +0000

    Base models

--- niblr/models.py
+++ niblr/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth import User
+from geoposition.fields import GeopositionField
+
+
+CHEAP = 0
+MIDRANGE = 1
+EXPENSIVE = 2
+PRICE_CLASSIFICATION = (
+    (CHEAP, 'Low'),
+    (MIDRANGE, 'Normal'),
+    (HIGH, 'High'),
+)
 
 # Create your models here.
+class Cuisine(models.Model):
+    name = models.CharField(max_length=64)
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=64)
+    position = GeopositionField()
+    cuisine_category = models.ManyToManyField()
+    price_classification = models.CharField(
+        choices=PRICE_CLASSIFICATION,
+        default=MIDRANGE
+    )
+
+
+class UserRating(models.Model):
+    star_rating = models.IntegerField(default=5)
+    comments = models.TextField(max_length=140)
+    author = models.ForeignKey(User)
+
