9b56cdaefc71c05e384d9a307e797aa774840f88
Seun Dawodu <seun.dawodu@artsalliancemedia.com>
2017-03-13 10:34:16 +0000

    Generic WIP

--- niblr/models.py
+++ niblr/models.py
@@ -3,18 +3,21 @@ from django.contrib.auth.models import User
 from geoposition.fields import GeopositionField
 
 
-CHEAP = 0
-MIDRANGE = 1
-EXPENSIVE = 2
+CHEAP = '0'
+MIDRANGE = '1'
+EXPENSIVE = '2'
 PRICE_CLASSIFICATION = (
-    (CHEAP, 'Low'),
-    (MIDRANGE, 'Normal'),
-    (EXPENSIVE, 'High'),
+    (CHEAP, 'Cheap'),
+    (MIDRANGE, 'Mid-range'),
+    (EXPENSIVE, 'Expensive'),
 )
 
 
 class Cuisine(models.Model):
     name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=64)
@@ -26,10 +29,16 @@ class Restaurant(models.Model):
         max_length=12,
     )
 
+    def __str__(self):
+        return self.name
+
 
 class UserRating(models.Model):
     restaurant = models.ForeignKey(Restaurant)
     star_rating = models.IntegerField(default=5)
     comments = models.TextField(max_length=140)
     author = models.ForeignKey(User)
+
+    def __str__(self):
+        return "{0}-{1}".format(self.author, self.restaurant)
 
